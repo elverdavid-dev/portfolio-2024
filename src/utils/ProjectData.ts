0b32f054762edaab543fba5fc36f5faf5a358222
Elver David <elverdavid0839@gmail.com>
2024-01-13 14:52:31 -0500

    se cambio las imagenes de los projectos

--- src/utils/ProjectData.ts
+++ src/utils/ProjectData.ts
@@ -12,7 +12,7 @@ export const ProjectData: ProjectInterface[] = [
     description:
       'Una plataforma de recetas desarrollada usando Next.js, TypeScript y Tailwind CSS en el frontend, junto con mi propia API. Ofrece búsquedas por nombre, categoría o país, y brinda información completa de cada receta. Esta creación demuestra mi habilidad para construir soluciones web interactivas y funcionales.',
     urlImage:
-      'https://res.cloudinary.com/dnbdpnvz1/image/upload/f_auto,q_auto/v1/images_project/jzpugkctni4pktg3519x',
+      'https://res.cloudinary.com/dnbdpnvz1/image/upload/f_auto,q_auto/v1/images_project/portafolio-images/gxnlh9gwj5v2obt7eaj9',
     urlGithub: 'https://github.com/ElverDavid07/Project_Recipe_Page',
     urlWeb: 'https://www.globalfood.site'
   },
@@ -30,7 +30,7 @@ export const ProjectData: ProjectInterface[] = [
     description:
       'Diseño de Landig page, Con un enfoque en la estética y la experiencia del usuario.El diseño es optimizado y responsive, asegurando una visualización perfecta en diferentes dispositivos. ',
     urlImage:
-      'https://res.cloudinary.com/dnbdpnvz1/image/upload/f_auto,q_auto/v1/images_project/mockup/zunsuxdv62br5wnaofuu',
+      'https://res.cloudinary.com/dnbdpnvz1/image/upload/f_auto,q_auto/v1/images_project/portafolio-images/qakx2xxajxda2xu0kl5b',
     urlGithub: 'https://github.com/ElverDavid07/Landing_Page',
     urlWeb: 'https://landing-page-psi-flax.vercel.app'
   },
@@ -39,7 +39,7 @@ export const ProjectData: ProjectInterface[] = [
     description:
       'Este proyecto es una página web informativa sobre una compañía exportadora de frutas. En esta página, los usuarios pueden encontrar información sobre la compañía, sus productos y cómo contactarlos. Es importante señalar que esta página no es oficial de la compañía, sino que fue creada por mí como parte de la puesta en práctica de mis conocimientos y habilidades en el desarrollo web.',
     urlImage:
-      'https://res.cloudinary.com/dnbdpnvz1/image/upload/f_auto,q_auto/v1/images_project/mockup/rdxa1qnqvrfordezhoaj',
+      'https://res.cloudinary.com/dnbdpnvz1/image/upload/f_auto,q_auto/v1/images_project/portafolio-images/m7joilzbzayoukxvvqho',
     urlGithub: 'https://github.com/ElverDavid07/static-page',
     urlWeb: 'https://www.colfrutas.site'
   }
