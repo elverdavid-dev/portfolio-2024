4e9fcf9ed2ec98b92e27a3504cd1dbffe87c98b8
Elver David <elverdavid0839@gmail.com>
2024-02-12 21:25:43 -0500

    nuevo badge

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -11,6 +11,19 @@ export default {
 				primary: '#06A0F9',
 				base: '#0d1117',
 			},
+			animation: {
+				'background-shine': 'background-shine 2s linear infinite',
+			},
+			keyframes: {
+				'background-shine': {
+					from: {
+						backgroundPosition: '0 0',
+					},
+					to: {
+						backgroundPosition: '-200% 0',
+					},
+				},
+			},
 		},
 	},
 	plugins: [require('tailwindcss-animated')],
