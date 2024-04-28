28b17fdc052ba6b050d33b30c87e03201bb41bf5
elver david <elverdavid0839@gmail.com>
2024-04-28 15:35:05 -0500

    refactorizacion y mejora en el diseño

--- src/utils/CertificateData.ts
+++ src/utils/CertificateData.ts
@@ -1,35 +1,38 @@
 export interface CertificateInterface {
-  name: string
-  urlImage: string
-  urlPdf: string
-  nameUniversity: string
-  course: string
+	name: string
+	urlImage: string
+	urlPdf: string
+	nameUniversity: string
+	course: string
 }
 
 export const CertificatesData: CertificateInterface[] = [
-  {
-    name: 'certificate all course',
-    urlImage:
-      'https://res.cloudinary.com/dnbdpnvz1/image/upload/f_auto,q_auto/v1/images_project/certificates_images/vyigzzqagciro3ljmqnh',
-    urlPdf: '/certificates/certificate-all-course.pdf',
-    nameUniversity: 'Universidad autonoma de bucaramanga',
-    course: 'diplomado mision tic 2022'
-  },
-  {
-    name: 'certificate web developer',
-    urlImage:
-      'https://res.cloudinary.com/dnbdpnvz1/image/upload/f_auto,q_auto/v1/images_project/certificates_images/bt9hgpe7p3fqvq6hxh52',
-    urlPdf: '/certificates/certificate-web-developer.pdf',
-    nameUniversity: 'Universidad autonoma de bucaramanga',
-    course: 'Desarrollo web'
-  },
-  {
-    name: 'certificate tecnico laboral',
-    urlImage:
-      'https://res.cloudinary.com/dnbdpnvz1/image/upload/f_auto,q_auto/v1/images_project/certificates_images/wrqlafxtcgimzegdky52',
-    urlPdf: '/certificates/certificado-tecnico-laboral.pdf',
-    nameUniversity: 'Universidad autonoma de bucaramanga',
-    course:
-      'Tecnico laboral en habilidades en programacion con enfasis en aplicaciones web'
-  }
+	{
+		name: 'certificate all course',
+		urlImage:
+			'https://res.cloudinary.com/dnbdpnvz1/image/upload/f_auto,q_auto/v1/images_project/certificates_images/vyigzzqagciro3ljmqnh',
+		urlPdf:
+			'https://res.cloudinary.com/dnbdpnvz1/image/upload/f_auto,q_auto/v1/images_project/portafolio-images/pdf-portafolio/ehmcg0sq10dbf4ozemrk',
+		nameUniversity: 'Universidad autonoma de bucaramanga',
+		course: 'diplomado mision tic 2022',
+	},
+	{
+		name: 'certificate web developer',
+		urlImage:
+			'https://res.cloudinary.com/dnbdpnvz1/image/upload/f_auto,q_auto/v1/images_project/certificates_images/bt9hgpe7p3fqvq6hxh52',
+		urlPdf:
+			'https://res.cloudinary.com/dnbdpnvz1/image/upload/f_auto,q_auto/v1/images_project/portafolio-images/pdf-portafolio/jrreimmxlgteiovjxgs9',
+		nameUniversity: 'Universidad autonoma de bucaramanga',
+		course: 'Desarrollo web',
+	},
+	{
+		name: 'certificate tecnico laboral',
+		urlImage:
+			'https://res.cloudinary.com/dnbdpnvz1/image/upload/f_auto,q_auto/v1/images_project/certificates_images/wrqlafxtcgimzegdky52',
+		urlPdf:
+			'https://res.cloudinary.com/dnbdpnvz1/image/upload/f_auto,q_auto/v1/images_project/portafolio-images/pdf-portafolio/qxtriurb9o3nqb0iyni9',
+		nameUniversity: 'Universidad autonoma de bucaramanga',
+		course:
+			'Tecnico laboral en habilidades en programacion con enfasis en aplicaciones web',
+	},
 ]
--- src/utils/ProjectData.ts
+++ src/utils/ProjectData.ts
@@ -1,52 +1,49 @@
 export interface ProjectInterface {
-  name: string
-  description: string
-  urlImage: string
-  urlGithub: string
-  urlWeb: string | null
-  stack?:string[]
+	name: string
+	description: string
+	urlImage: string
+	urlGithub?: string
+	urlWeb?: string
+	stack?: string[]
 }
 
 export const ProjectData: ProjectInterface[] = [
-  {
-    name: 'ChathubAi',
-    description:
-      'Estoy trabajando en el desarrollo de un chatbot que utiliza la tecnología de inteligencia artificial de OpenAI. Aunque todavía está en proceso de desarrollo, este chatbot utiliza el modelo GPT-4 para interactuar y responder preguntas de manera inteligente. ¡Estoy emocionado por seguir mejorándolo!',
-    urlImage:
-      'https://res.cloudinary.com/dnbdpnvz1/image/upload/f_auto,q_auto/v1/images_project/portafolio-images/aq5mly6klp2grdmz1few',
-    urlGithub: 'https://github.com/ElverDavid07/chathub-ai',
-    urlWeb: null,
-    stack:["icons/next.svg","icons/tailwind.svg","icons/typescript.svg"]
-  },
-  {
-    name: 'GlobalFood',
-    description:
-      'Desarrollé una plataforma donde puedes encontrar recetas deliciosas de todo el mundo. Puedes buscar recetas por su nombre, el tipo de comida o el país de origen. Cada receta viene con detalles completos, como los ingredientes y las instrucciones paso a paso para cocinarla.',
-    urlImage:
-      'https://res.cloudinary.com/dnbdpnvz1/image/upload/f_auto,q_auto/v1/images_project/portafolio-images/gxnlh9gwj5v2obt7eaj9',
-    urlGithub: 'https://github.com/ElverDavid07/Project_Recipe_Page',
-    urlWeb: 'https://www.globalfood.site',
-    stack:["icons/next.svg","icons/tailwind.svg","icons/typescript.svg"]
-  },
-  {
-    name: 'Recipes Api',
-    description:
-      'Desarrollé un sistema en línea para gestionar recetas de cocina. Con esta herramienta, puedes buscar recetas por nombre, explorar diferentes categorías de platos y ver las últimas recetas agregadas. Además, incluye características como paginación para navegar fácilmente por las recetas y validación de datos para garantizar que la información sea precisa y segura.',
-    urlImage:
-      'https://res.cloudinary.com/dnbdpnvz1/image/upload/f_auto,q_auto/v1/images_project/ljmt4nqma96xphu7iwfc',
-    urlGithub: 'https://github.com/ElverDavid07/Recipes-api',
-    urlWeb: 'https://www.recipesapi.online/docs',
-    stack:["icons/nestjs.svg","icons/mongodb.svg","icons/typescript.svg"]
-  },
-  {
-    name: 'Landing page',
-    description:
-      'Página de inicio con un diseño enfocado en la apariencia y la facilidad de uso. Esta página está diseñada para ser atractiva visualmente y brindar una experiencia óptima para los visitantes. Fue uno de mis primeros proyectos ',
-    urlImage:
-      'https://res.cloudinary.com/dnbdpnvz1/image/upload/f_auto,q_auto/v1/images_project/portafolio-images/qakx2xxajxda2xu0kl5b',
-    urlGithub: 'https://github.com/ElverDavid07/Landing_Page',
-    urlWeb: 'https://landing-page-psi-flax.vercel.app',
-    stack:["icons/astro.svg","icons/tailwind.svg","icons/typescript.svg"]
-  },
-
+	{
+		name: 'Delivery food page',
+		description:
+			'Este es un proyecto personal en desarrollo, una plataforma de delivery de alimentos, diseñada específicamente para un restaurante. En esta plataforma, los usuarios podrán explorar el menú del restaurante y realizar pedidos directamente desde la comodidad de su hogar.',
+		urlImage:
+			'https://res.cloudinary.com/dnbdpnvz1/image/upload/f_auto,q_auto/v1/images_project/portafolio-images/sh9kvs69jigy4fwyhjro',
+		stack: ['icons/next.svg', 'icons/tailwind.svg', 'icons/typescript.svg'],
+	},
+	{
+		name: 'GlobalFood',
+		description:
+			'Desarrollé una plataforma donde puedes encontrar recetas deliciosas de todo el mundo. Puedes buscar recetas por su nombre, el tipo de comida o el país de origen. Cada receta viene con detalles completos, como los ingredientes y las instrucciones paso a paso para cocinarla.',
+		urlImage:
+			'https://res.cloudinary.com/dnbdpnvz1/image/upload/f_auto,q_auto/v1/images_project/portafolio-images/zvefq7w4x4uheqpquzt8',
+		urlGithub: 'https://github.com/ElverDavid07/Project_Recipe_Page',
+		urlWeb: 'https://www.globalfood.site',
+		stack: ['icons/next.svg', 'icons/tailwind.svg', 'icons/typescript.svg'],
+	},
+	{
+		name: 'Recipes Api',
+		description:
+			'Desarrollé un sistema en línea para gestionar recetas de cocina. Con esta herramienta, puedes buscar recetas por nombre, explorar diferentes categorías de platos y ver las últimas recetas agregadas. Además, incluye características como paginación para navegar fácilmente por las recetas y validación de datos para garantizar que la información sea precisa y segura.',
+		urlImage:
+			'https://res.cloudinary.com/dnbdpnvz1/image/upload/f_auto,q_auto/v1/images_project/ljmt4nqma96xphu7iwfc',
+		urlGithub: 'https://github.com/ElverDavid07/Recipes-api',
+		urlWeb: 'https://www.recipesapi.online/docs',
+		stack: ['icons/nestjs.svg', 'icons/mongodb.svg', 'icons/typescript.svg'],
+	},
+	{
+		name: 'Landing page',
+		description:
+			'Página de inicio con un diseño enfocado en la apariencia y la facilidad de uso. Esta página está diseñada para ser atractiva visualmente y brindar una experiencia óptima para los visitantes. Fue uno de mis primeros proyectos ',
+		urlImage:
+			'https://res.cloudinary.com/dnbdpnvz1/image/upload/f_auto,q_auto/v1/images_project/portafolio-images/qakx2xxajxda2xu0kl5b',
+		urlGithub: 'https://github.com/ElverDavid07/Landing_Page',
+		urlWeb: 'https://landing-page-psi-flax.vercel.app',
+		stack: ['icons/astro.svg', 'icons/tailwind.svg', 'icons/typescript.svg'],
+	},
 ]
