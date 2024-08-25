ebcc9301bc2bc4614959cb5383c8fa30cec1e7ea
Elver David <elverdavid0839@gmail.com>
2024-08-25 14:36:02 -0500

    chore: add new project

--- src/utils/ProjectData.ts
+++ src/utils/ProjectData.ts
@@ -14,14 +14,22 @@ export interface ProjectInterface {
 	urlWeb?: string
 	stack?: ImageMetadata[]
 }
-
+const base_url = "https://res.cloudinary.com/dnbdpnvz1/image/upload/f_auto,q_auto/v1/images_project/portafolio-images"
 export const ProjectData: ProjectInterface[] = [
+	{
+		name: 'Gamma movies',
+		description:
+			'La página presenta secciones de películas populares, próximas, mejor valoradas y en cartelera, además de funcionalidades como búsqueda, paginación, detalles de películas y autenticación.',
+		urlImage: `${base_url}/t9hzgeuzn2lvmeswvbxp`,
+		urlWeb: 'https://gamma-movies.vercel.app/',
+		stack: [nextIcon, tailwindIcon, typescriptIcon],
+	},
 	{
 		name: 'Delivery food page',
 		description:
 			'Este es un proyecto personal en desarrollo, una plataforma de delivery de alimentos, diseñada específicamente para un restaurante. En esta plataforma, los usuarios podrán explorar el menú del restaurante y realizar pedidos directamente desde la comodidad de su hogar.',
 		urlImage:
-			'https://res.cloudinary.com/dnbdpnvz1/image/upload/f_auto,q_auto/v1/images_project/portafolio-images/sh9kvs69jigy4fwyhjro',
+			`${base_url}/sh9kvs69jigy4fwyhjro`,
 		stack: [nextIcon, tailwindIcon, typescriptIcon],
 	},
 	{
@@ -29,7 +37,7 @@ export const ProjectData: ProjectInterface[] = [
 		description:
 			'Desarrollé una plataforma donde puedes encontrar recetas deliciosas de todo el mundo. Puedes buscar recetas por su nombre, el tipo de comida o el país de origen. Cada receta viene con detalles completos, como los ingredientes y las instrucciones paso a paso para cocinarla.',
 		urlImage:
-			'https://res.cloudinary.com/dnbdpnvz1/image/upload/f_auto,q_auto/v1/images_project/portafolio-images/enhone6togoeczjwslsk',
+			`${base_url}/enhone6togoeczjwslsk`,
 		urlGithub: 'https://github.com/ElverDavid07/Project_Recipe_Page',
 		urlWeb: 'https://www.globalfood.site',
 		stack: [nextIcon, tailwindIcon, typescriptIcon],
@@ -39,7 +47,7 @@ export const ProjectData: ProjectInterface[] = [
 		description:
 			'Desarrollé un sistema en línea para gestionar recetas de cocina. Con esta herramienta, puedes buscar recetas por nombre, explorar diferentes categorías de platos y ver las últimas recetas agregadas. Además, incluye características como paginación para navegar fácilmente por las recetas y validación de datos para garantizar que la información sea precisa y segura.',
 		urlImage:
-			'https://res.cloudinary.com/dnbdpnvz1/image/upload/f_auto,q_auto/v1/images_project/ljmt4nqma96xphu7iwfc',
+			`${base_url}/rr5f6fwfllnykm9ijlie`,
 		urlGithub: 'https://github.com/ElverDavid07/Recipes-api',
 		urlWeb: 'https://www.recipesapi.online/docs',
 		stack: [nestIcon, mongodbIcon, typescriptIcon],
@@ -49,7 +57,7 @@ export const ProjectData: ProjectInterface[] = [
 		description:
 			'Página de inicio con un diseño enfocado en la apariencia y la facilidad de uso. Esta página está diseñada para ser atractiva visualmente y brindar una experiencia óptima para los visitantes. Fue uno de mis primeros proyectos ',
 		urlImage:
-			'https://res.cloudinary.com/dnbdpnvz1/image/upload/f_auto,q_auto/v1/images_project/portafolio-images/qakx2xxajxda2xu0kl5b',
+			`${base_url}/qakx2xxajxda2xu0kl5b`,
 		urlGithub: 'https://github.com/ElverDavid07/Landing_Page',
 		urlWeb: 'https://landing-page-psi-flax.vercel.app',
 		stack: [astroIcon, tailwindIcon, typescriptIcon],
